4927fe1c89ce209cd94ac5a1351760c13537aaf4
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-04-13 12:55:24 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [newTask, setnewTask] = useState("");
+  const [appTasks, appTask] = useState([]);
+
+  const taskInputHandler = (enteredText) => {
+    setnewTask(enteredText);
+  };
+
+  const addTaskHandler = () => {
+    appTask((currentTask) => [...currentTask, newTask]);
+    console.log(newTask);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Write a Task here"
+          style={styles.input}
+          onChangeText={taskInputHandler}
+          value={newTask}
+        />
+        <View style={styles.plus}>
+          <Button title="+" onPress={addTaskHandler} />
+        </View>
+      </View>
+      <View>
+        {appTasks.map((task) => (
+          <Text style={styles.list} key={task}>
+            {task}
+          </Text>
+        ))}
+      </View>
+      <View style={styles.totalTask}>
+        <Button
+          title="total tasks"
+          onPress={() => alert(`Total tasks: ${appTasks.length}`)}
+        />
+      </View>
     </View>
   );
 }
@@ -14,8 +48,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  list: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: "#fafafa",
+  },
+  totalTask: {
+    backgroundColor: "lightblue",
+    padding: 10,
+    borderWidth: 3,
+    borderColor: "#fafafa",
+  },
+  plus: {
+    borderWidth: 4,
+    borderRadius: 100,
+    backgroundColor: "black",
+    borderColor: "#fafafa",
+    margin: 20,
+    color: "#fff",
+  },
+  input: {
+    borderColor: "pink",
+    borderWidth: 4,
+    padding: 10,
+    borderRadius: 20,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+    bottom: 20,
   },
 });
